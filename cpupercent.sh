fad0522dabca764043f0d55ef0aec8e6972295f3
dfuehrer <dfuehrer@email.arizona.edu>
2021-03-23 19:40:05 -0700

    made an edit script to open up a file correctly in a terminal text editor and resize it correctly after its open cause thats an issue i have, started using it in my scripts that open files in text editors

--- cpupercent.sh
+++ cpupercent.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 case "$BLOCK_BUTTON" in
     1) "$TERMINAL" -e htop ;;
-    2) "$TERMINAL" -e "$EDITOR" "$0" ;;
+    2) edit "$0" ;;
     3) "$TERMINAL" -e bpytop ;;
     '') cpucercent | tr -d '\n';;
 esac
--- stalonetray.sh
+++ stalonetray.sh
@@ -3,6 +3,6 @@
 geometry="1x1+$(( 1920-$(xwininfo -root | sed -n 's/^.*id: 0x.*"\(.*\)"/\1/p' | ansifilter | wc -m)*8+4-8 ))+20"
 case "$BLOCK_BUTTON" in
     1) [ $p = v ] && { setsid -f stalonetray --geometry $geometry && pkill -RTMIN+27 dwmblocks; } || { killall -e stalonetray; pkill -RTMIN+27 dwmblocks; };;
-    2) "$TERMINAL" -e "$EDITOR" "$0" ;;
+    2) edit "$0" ;;
     '') printf $p;;
 esac
