cc007405706378955a7986347f9bb3e877550e99
dfuehrer <dfuehrer@email.arizona.edu>
2022-08-29 01:50:31 -0700

    updates to bunch of scripts, found out there are already progress bars in dunst

--- nastart.sh
+++ nastart.sh
@@ -4,12 +4,15 @@
 comList="
 barrier ${XDG_DATA_HOME:-$HOME/.local/share}/barrier/barrier.png
 keynav.sh ${XDG_DATA_HOME:-$HOME/.local/share}/nastart/keynav.png
+run_nb ${XDG_DATA_HOME:-$HOME/.local/share}/nastart/newsboat.png
 screentranslator ${XDG_DATA_HOME:-$HOME/.local/share}/nastart/screentranslator.png
 brave /usr/lib/brave-bin/product_logo_256.png
 firefox /usr/share/icons/hicolor/384x384/apps/firefox.png
 "
+run_nb () {
+    "$TERMINAL" -e newsboat
+}
 comList=$(echo "$comList" | grep -Ex '[^[:space:]]+\>.*\<[^[:space:]]+')      # get rid of lines that are not in this format
-echo "$comList"
 # com is just the progam that was selected in the thumbnail view of sxiv
 com=$(echo "$comList" | awk "/$(echo "$comList" | cut -d ' ' -f 2- | sxiv -iot | tr '\n' '|' | sed 's/|$//; s|/|\\/|g')+/"' {print $1}')      # pass sxiv the path to the pics and then awk out the output for just the program in comList.  tr makes it so the options are in one line as ORs.  sed cleans up and makes it so the / are for paths not for awk
 # then run the programs probably
--- restartprocess.sh
+++ restartprocess.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-eval $(echo 'flags: h; parameters: s,sig;' | clparser "$@")
+clargs='flags: h; parameters: s,sig;'
+eval $(echo "$clargs" | clparser "$@")
+[ $h ] && echo $clargs && exit
 #echo $s "$defaults" $(pgrep $defaults) | tee -a ~/rp.txt
 # TODO this is lazy so change it, dont really need to do this for all but whatever
 #[ "$(pgrep ${defaults% *})" ] && echo its already handled im good || { echo gonna start "$defaults" && "$defaults"; } &
 baseprog="${defaults%% *}"
 echo "$baseprog"
 [ -z "$(pgrep $baseprog)" ] && echo "i shouldnt be doing this" && $defaults &
-[ $s ] && { echo "using sig" && pkill "$baseprog" -"$s"; } || { echo "killing all" && killall $baseprog; setsid -f $defaults; }
+[ $s ] && { echo "using sig" "$s" && pkill "$baseprog" -"$s"; } || { echo "killing all" && killall $baseprog; setsid -f $defaults; }
 #echo done >> ~/rp.txt
